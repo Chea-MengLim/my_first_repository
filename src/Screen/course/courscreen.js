5d0d9f0d5b7d4acbbf8119de07d9f56b5bff4105
Chea-MengLim <menglimchea@gmail.com>
2023-06-26 22:15:52 +0700

    updated screen

--- src/Screen/course/courscreen.js
+++ src/Screen/course/courscreen.js
@@ -1,6 +1,6 @@
 const CourseScreen = () => {
     return (
-        <div> Hi list course and I have updated this code </div>
+        <div> Hi list course and I have updated this code now I updated again </div>
     )
 }
 
